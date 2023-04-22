07c016b3eda43751e71d0866edf145db4d3b1a62
Xzou0011 <106866015+Xzou0011@users.noreply.github.com>
2023-04-22 16:39:44 +1000

    Initial commit; homepage; emo webpage basic;

---
+++ src/main/java/com/qualityeducation/bullyedu/BullyeduApplication.java
@@ -0,0 +1,13 @@
+package com.qualityeducation.bullyedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullyeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullyeduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qualityeducation/bullyedu/BullyeduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualityeducation.bullyedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullyeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
