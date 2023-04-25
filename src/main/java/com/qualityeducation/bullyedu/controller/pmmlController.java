1b52ab14f05b51b2607630a2275d8997093ad96f
Xzou0011 <106866015+Xzou0011@users.noreply.github.com>
2023-04-26 01:53:25 +1000

    pmml able to run in spring boot, with user input and display output next step: frontend for homepage and emo webpage

---
+++ src/main/java/com/qualityeducation/bullyedu/controller/pmmlController.java
@@ -0,0 +1,48 @@
+package com.qualityeducation.bullyedu.controller;
+
+import com.qualityeducation.bullyedu.service.pmmlService;
+import org.dmg.pmml.FieldName;
+import org.jpmml.evaluator.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class pmmlController {
+
+//    @GetMapping("emotional")
+//    public String greetingForm() {
+//        return "emotional";
+//    }
+    @Autowired
+    private pmmlService service;
+
+    @GetMapping("/emotional")
+    public String inti() {
+        return "emotional";
+    }
+
+    @PostMapping ("/emotional")
+    public String search(@RequestParam(name = "inputString",required = false) String inputString, Model model) throws JAXBException, IOException, SAXException {
+        int primitiveValue = service.search(inputString);
+        model.addAttribute("result", primitiveValue);
+        return "emotional";
+
+//        List<Math> mathList = service.search(keyword);
+//        model.addAttribute("mathList", mathList);
+//        return "math";
+    }
+
+
+}
--- src/main/java/com/qualityeducation/bullyedu/service/pmmlService.java
+++ src/main/java/com/qualityeducation/bullyedu/service/pmmlService.java
@@ -1,42 +1,42 @@
-package com.qualityeducation.bullyedu.controller;
-
-import org.jpmml.evaluator.*;
-import org.xml.sax.SAXException;
-import javax.xml.bind.JAXBException;
-import java.io.*;
-import java.util.List;
+package com.qualityeducation.bullyedu.service;
 
 import org.dmg.pmml.FieldName;
+import org.jpmml.evaluator.*;
+import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
+import javax.xml.bind.JAXBException;
+import java.io.File;
 import java.io.IOException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
-public class JpmmlExample {
-
-    public static void main(String[] args) throws IOException, SAXException, JAXBException {
+@Service
+public class pmmlService {
+    public int search(String keyword) throws JAXBException, IOException, SAXException {
         Evaluator evaluator = new LoadingModelEvaluatorBuilder()
                 .load(new File("/Users/zouxin/Desktop/copy/src/main/resources/demo2.pmml"))
                 .build();
         evaluator.verify();
         List<InputField> inputFields = evaluator.getInputFields();
         List<TargetField> targetFields = evaluator.getTargetFields();
-
         Map<FieldName, FieldValue> arguments = new LinkedHashMap<>();
-        Object rawValue = "something";
-        arguments.put(inputFields.get(0).getName(), inputFields.get(0).prepare(rawValue));
-        System.out.println(arguments.isEmpty());
+        arguments.put(inputFields.get(0).getName(), inputFields.get(0).prepare(keyword));
+        System.out.println(arguments);
+
         Map<FieldName, ?> results = evaluator.evaluate(arguments);
 
         TargetField targetField0 = targetFields.get(0);
         FieldName targetFieldName = targetField0.getName();
         Object targetFieldValue = results.get(targetFieldName);
 
-        int primitiveValue = 0;
+        int primitiveValue = 6;
         if (targetFieldValue instanceof Computable computable) {
             primitiveValue = (Integer)computable.getResult();
         }
 
         System.out.println("Predicted value" + primitiveValue);
+        return primitiveValue;
     }
 }
