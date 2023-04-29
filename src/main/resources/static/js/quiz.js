3a4ccc87fc7402e8973b18cac80f27530db74695
Xzou0011 <106866015+Xzou0011@users.noreply.github.com>
2023-04-29 18:16:35 +1000

    quiz page modified; add result button; type validation; and three different types basic page

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -3,54 +3,35 @@ const responseTexts = {
   checkbox1:
     "Rethink about yourself: Think about the consequences of these behaviours caused to others.",
   checkbox2:
-    "Offer support: Showing support and empathy towards the victim, and letting them know that you care and are willing to support them.",
-  checkbox3:
     "Ignoring: You can choose to ignore them if you want to. It will not cause any harm to you if you ignore these comments. ",
-  checkbox4:
-    "Speak up: Good! Protect yourself! Tell the individuals that their actions have done nothing wrong.",
-  checkbox5:
+  checkbox3:
     "Reach out: Depending on the seriousness of the situation, you can choose to directly speak up to help or via indirect methods like reporting to the higher authorities at the back.",
 };
 // question2
 const responseTexts2 = {
   checkbox1:
-    "Lucky! However, we can offer direct or indirect help if needed but depending on the situation. Never be a bystander of the situation.",
-  checkbox2:
     "Practise good communication skills: Practise active listening, express yourself assertively without resorting to bullying tactics, and strive to resolve conflicts in a peaceful and respectful manner.",
-  checkbox3:
-    "Stop and confront: Hey, that's not good behaviour! Reflect on your behaviour when bullying others and think about the consequences of the behaviour. Think about how you would feel if others did the same to you.",
-  checkbox4:
-    "Speak up and support peers: Good！Protect yourself! Tell the bullied that they have done nothing wrong. Positive encouragement and support may be a real help to them.",
-  checkbox5:
+  checkbox2:
     "Avoid bystanding: Never be a bystander when encountering these situations. Offer helps to the individuals who were being targeted.",
+  checkbox3:
+    "Seek professional help: Talk to a counsellor or therapist to help to identify and seek ways on learning effective communication and conflict resolution skills, which can reduce the likelihood of engaging in bullying behaviour.",
 };
 // question3
 const responseTexts3 = {
   checkbox1:
-    "It’s good to know you are not involved in bullying. And make sure never stand by and watch or encourage bullying behaviour.",
+    "Speak up: You can calmly and assertively let them know that what they are doing is wrong and that it needs to stop.",
   checkbox2:
-    "Seek professional help: If the bullying has a significant impact on your mental health and well-being, consider seeking support from a mental health professional, such as a therapist or counsellor.",
-  checkbox3:
-    'Be Assertive: Practise assertiveness skills, which help you be prepared to stand up for yourself in a calm and confident manner. Use "I" statements to express your feelings and set clear boundaries with the person who is bullying you.',
-  checkbox4:
     "Educate and reflect personal behaviour: Learn about the consequences of bullying and apologise to victims and reflect on your past behaviour. Recognize that bullying behaviour is harmful and can have serious consequences such as potential legal and ethical consequences.",
+  checkbox3:
+    "Seek professional help: If the bullying has a significant impact on your mental health and well-being, consider seeking support from a mental health professional, such as a therapist or counsellor.",
 };
-
 // question4
 const responseTexts4 = {
   checkbox1:
-    "No additional action taken: It’s good to know you are not involved in bullying. Hope that you will never stand by and watch or encourage bullying behaviour.",
-  checkbox2:
-    "Speak up: If you witness social bullying, it is important to speak up and let the bully know that their behaviour is not acceptable.",
-  checkbox3:
-    "Reflect yourself: Reflect on the past experience and change the negative behaviour to positive behaviour to reduce and prevent it from happening.",
-  checkbox4:
     "Seek professional help: Consider talking to a counsellor or therapist about the bullying. They can help you work through your feelings and develop strategies for dealing with the situation.",
-  checkbox5:
+  checkbox2:
     "Seek for help: If it is difficult to control the bullying behaviour, consider seeking help from a mentor, counsellor, or mental health professional. They can provide you with support, guidance, and strategies to help you change your behaviour in a positive way.",
-  checkbox6:
-    "Be Assertive: Practise assertiveness skills, which help you be prepared to stand up for yourself in a calm and confident manner. Fighting back can stop bullying, but it's not a good way to solve problems,however communication can.",
-  checkbox7:
+  checkbox3:
     "Offer support: Showing support and empathy towards the victim, and letting them know that you care and are willing to support them.",
 };
 
@@ -62,20 +43,14 @@ const responseTexts5 = {
     "Tip: It’s good to know you are not involved in bullying. And make sure never stand by and watch or encourage bullying behaviour.",
   checkbox3:
     "Acknowledge your behaviour: It is important to recognize that your actions are not acceptable and take responsibility for them. Acknowledge the harm you have caused and commit to changing your behaviour.",
-  checkbox4:
-    "Having more sympathy: Sometimes you are not intended to hurt someone, but the actions end up hurting others. Make sure you think before you act.",
-  checkbox5:
-    "Stand up for yourself: It can be difficult, but standing up for yourself can be effective in stopping the bullying. Use assertive language and let the bully know that their behaviour is not acceptable.",
 };
 // question6
 const responseTexts6 = {
   checkbox1:
-    "Avoid being alone: If possible, try to avoid being alone with the bully. Stick with a group of friends or stay in public areas where there are people around.",
-  checkbox2:
     "Tip: It’s good to know you are not involved in bullying. However, please ensure that you will never be the bystanders or indirect bully in such situations.",
-  checkbox3:
+  checkbox2:
     "A new way to show off: In sports programs, competition is part of the game. Coaches and leaders can focus on teamwork and skill-building to bring out the best in their students. In sports games you can show your power and skills under a rule and focus more on teamwork to achieve some common target. Also it can be a good chance to help other people who might not be as strong as you to build their body and grow together.",
-  checkbox4:
+  checkbox3:
     "Report the bullying: If you witness bullying or mistreatment, report it to a teacher, counsellor, or other trusted adult. This can help prevent further incidents and ensure that the victim receives the support they need.",
 };
 
@@ -94,13 +69,9 @@ const responseTexts8 = {
   checkbox1:
     "Keep evidence of being bullied: Keep a record of the bullying incidents, including dates, times, and details of what happened. It is essential and useful if you need to report the bullying to university or relevant authorities.",
   checkbox2:
-    "Set boundaries: Make it clear to your friends that you won’t be involved in any negative behaviour and let others know what you are and are not willing to accept, and stick to your boundaries.",
-  checkbox3:
     "Reflect on personal actions: Apologise to victims and reflect on your past behaviour. Recognize that bullying behaviour is harmful and can have serious consequences for the victim, as well as for the bully.",
-  checkbox4:
+  checkbox3:
     "Speak up: If you witness direct or indirect bullying, it is important to speak up and let the bully know that their behaviour is not acceptable.",
-  checkbox5:
-    "Offer support: Offer to help the victim in any way you can, whether it's offering to walk with them to class, sitting with them at lunch, or just being there to listen.",
 };
 
 // question9
@@ -118,12 +89,10 @@ const responseTexts10 = {
   checkbox1:
     "Report bullying: If you witness bullying or mistreatment, report it to a counsellor, relevant government authorities, etc., depending on the seriousness of the situation. E.g.: if the bullying is serious, report it to the police; if the bullying occurs on Facebook, report it to Facebook.",
   checkbox2:
-    "Offer support: Support the person who is being bullied to ask for help e.g. go with them to a place they can get help or provide them with information about where to go for help.",
-  checkbox3:
     "Stand up and speak up: Don't be afraid to speak up for yourself when you feel like you are being pushed around. Use a calm and assertive tone, and let others know how you feel and what you need from them.",
-  checkbox4:
+  checkbox3:
     "Protect yourself: Don’t let yourself be the new target of bullying and get away from the situation.",
-  checkbox5:
+  checkbox4:
     "Report bullying: If you witness bullying or mistreatment, report it to a teacher, counsellor, relevant government authorities, etc., depending on the seriousness of the situation. E.g.: if the bullying is serious, report it to the police; if the bullying occurs on Facebook, report it to Facebook.",
 };
 
@@ -138,16 +107,39 @@ const list8Checkboxes = document.querySelectorAll('input[name^="list8"]');
 const list9Checkboxes = document.querySelectorAll('input[name^="list9"]');
 const list10Checkboxes = document.querySelectorAll('input[name^="lista"]');
 
-console.log(list1Checkboxes);
-console.log(list2Checkboxes);
-console.log(list3Checkboxes);
-console.log(list4Checkboxes);
-console.log(list5Checkboxes);
-console.log(list6Checkboxes);
+//numbers
+let somenumber = 5;
+console.log("the intitla number is: " + somenumber);
+console.log("the victime is: number <= 3.5");
+console.log("the bystander is: 3.5 < number <= 6.5");
+console.log("the bully is: 6.5 < number");
+
+const submitBtn = document.getElementById("submit-btn");
+
+//something new here
+function checkAnswers() {
+  var questions = document.querySelectorAll("ul");
+  let countChecked = 0;
+  console.log("The updated number is: " + somenumber);
+
+  for (var i = 0; i < questions.length; i++) {
+    var question = questions[i];
+    var checked = question.querySelectorAll("input:checked");
+
+    if (checked.length === 1) {
+      countChecked++;
+    }
+    if (countChecked === 10) {
+      submitBtn.classList.remove("hide");
+    }
+  }
+}
+//end
 
 list1Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText1();
+    checkAnswers();
   });
 });
 
@@ -165,8 +157,20 @@ function updateResponseText1() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber + 1;
+      } else if (checkbox.id === "checkbox2") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 0.5;
+      }
+      // console.log("the current number is: " + somenumber);
+      // here
+      // here
       text += responseTexts[checkbox.id] + " ";
-      console.log(checkbox.id);
+      //
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -178,6 +182,8 @@ function updateResponseText1() {
 list2Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText2();
+
+    checkAnswers();
   });
 });
 
@@ -195,8 +201,21 @@ function updateResponseText2() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 0.5;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 1;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // 4.5 - 5   //    5 - 5.5
+      // here
       text += responseTexts2[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -208,9 +227,11 @@ function updateResponseText2() {
 list3Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText3();
+    checkAnswers();
   });
 });
 
+//question 3
 function updateResponseText3() {
   const checkedCheckboxes = document.querySelectorAll(
     'input[name^="list3"]:checked'
@@ -225,8 +246,20 @@ function updateResponseText3() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 0.5;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 1;
+      } else if (checkbox.id === "checkbox4") {
+        somenumber = somenumber - 1;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts3[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -238,6 +271,7 @@ function updateResponseText3() {
 list4Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText4();
+    checkAnswers();
   });
 });
 
@@ -255,8 +289,20 @@ function updateResponseText4() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 0.5;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts4[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -268,6 +314,7 @@ function updateResponseText4() {
 list5Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText5();
+    checkAnswers();
   });
 });
 
@@ -285,8 +332,18 @@ function updateResponseText5() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 1;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts5[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -298,6 +355,8 @@ function updateResponseText5() {
 list6Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText6();
+
+    checkAnswers();
   });
 });
 
@@ -315,8 +374,18 @@ function updateResponseText6() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 0.5;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts6[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -328,6 +397,8 @@ function updateResponseText6() {
 list7Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText7();
+
+    checkAnswers();
   });
 });
 
@@ -345,8 +416,18 @@ function updateResponseText7() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 1;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts7[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -358,6 +439,8 @@ function updateResponseText7() {
 list8Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText8();
+
+    checkAnswers();
   });
 });
 
@@ -375,8 +458,20 @@ function updateResponseText8() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 0.5;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts8[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -388,6 +483,8 @@ function updateResponseText8() {
 list9Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText9();
+
+    checkAnswers();
   });
 });
 
@@ -405,8 +502,20 @@ function updateResponseText9() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber - 1;
+      } else if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 1;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 0.5;
+      }
+      // console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts9[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
@@ -418,6 +527,8 @@ function updateResponseText9() {
 list10Checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     updateResponseText10();
+
+    checkAnswers();
   });
 });
 
@@ -435,12 +546,37 @@ function updateResponseText10() {
     });
     let text = "";
     checkedCheckboxes.forEach(function (checkbox) {
+      // here
+      // here
+      if (checkbox.id === "checkbox1") {
+        somenumber = somenumber + 0.2;
+      } else if (checkbox.id === "checkbox2") {
+        somenumber = somenumber + 0.5;
+      } else if (checkbox.id === "checkbox3") {
+        somenumber = somenumber + 0.5;
+      } else if (checkbox.id === "checkbox4") {
+        somenumber = somenumber + 0.5;
+      }
+      console.log("the number is: " + somenumber);
+      // here
+      // here
       text += responseTexts10[checkbox.id] + " ";
-      console.log(checkbox.id);
+
       suggestionBox.classList.remove("hide");
       responseText.textContent = text;
     });
   } else {
     responseText.textContent = "";
+  }
+}
+
+//submit button onclick change to anothe page
+function goResult() {
+  if (somenumber <= 3.5) {
+    window.location.href = "victim.html";
+  } else if (3.5 < somenumber <= 6.5) {
+    window.location.href = "bystander.html";
+  } else if (6.5 < somenumber) {
+    window.location.href = "bully.html";
   }
 }
